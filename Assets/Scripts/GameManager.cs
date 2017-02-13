2945714a0b0e9655999684ec051cfe4ef3633ddb
Alexander Xu <ajm.xu@outlook.com>
2017-02-13 20:03:19 +0000

    Initial Commit
    Curve Fever Replica Game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	private bool hasEnded = false;
+
+	void Awake() {
+		if (instance != null) {
+			Debug.LogError("Already another instance of GameManager in scene.");
+			return;
+		} else {
+			instance = this;
+		}
+	}
+
+	public void EndGame() {
+		if (!hasEnded) StartCoroutine(PlayerEndGameAnimation());
+		hasEnded = true;
+	}
+
+	private IEnumerator PlayerEndGameAnimation() {
+		Debug.Log("Game Over.");
+		yield return new WaitForSeconds(1f);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Snake : MonoBehaviour {
+	
+	public float speed = 3f;
+	public float rotationSpeed = 200f;
+
+	private float horizontal = 0f;
+
+	public string inputAxis = "Horizontal";
+
+	private GameManager gm;
+
+	void Start() {
+		gm = GameManager.instance;
+	}
+
+	void Update () {
+		horizontal = Input.GetAxisRaw(inputAxis);
+	}
+
+	void FixedUpdate () {
+		transform.Translate(Vector2.up * speed * Time.fixedDeltaTime, Space.Self);
+		transform.Rotate(Vector3.forward * -horizontal * rotationSpeed * Time.fixedDeltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.tag == "killsPlayer") {
+			speed = 0f;
+			rotationSpeed = 0f;
+
+			gm.EndGame();
+		}
+	}
+}
